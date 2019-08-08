1cae32bc0155dc69861b8efe72aaee38a353b6a7
Rizwan Ahmed <rizwan.ahmed@tintash.com>
2019-08-08 15:11:40 +0500

    Fix markTick overlapping issue
    What: fix issue of markTick overlapping
    Why:  context is not restored so lines are overlapping.
    Impact: low, it will not affect other part of the code

--- TemperatureDial/TemperatureDial/CircularView/CircularView.swift
+++ TemperatureDial/TemperatureDial/CircularView/CircularView.swift
@@ -19,7 +19,7 @@ class CircularView: UIView {
     //Stop angle: where we need to start the reading scale
     @IBInspectable var startAngle           :CGFloat    = 0.0
     @IBInspectable var stopAngle            :CGFloat    = 2.0 * pi
-        //total number of the calibrition lines need to add in between start and stop angle
+    //total number of the calibrition lines need to add in between start and stop angle
     @IBInspectable var numberOfOuterLines   :Int        = 60
     // margin from the UIview bounds
     @IBInspectable var margin               :CGFloat    = 20.0
@@ -45,40 +45,46 @@ class CircularView: UIView {
             ).insetBy(dx: margin * 1.5, dy: margin * 1.5))
         let transform = CGAffineTransform(translationX: bounds.width/2 - diameter/2 ,y: bounds.height/2 - diameter/2)        
         circle.apply(transform)
+        //        UIColor.yellow.setFill()
+        //        circle.fill()
+        UIColor.black.setStroke()
         circle.stroke()
         addGuageLines(context: context)
     }
     
     
     func addGuageLines(context : CGContext?) {
-
+        
         let diameter = min(bounds.width, bounds.height)
         let centerPoint = center
-        
+        context?.saveGState()
         for index in 0..<numberOfOuterLines {
-            let guageTickPath = UIBezierPath(rect: CGRect(x: centerPoint.x,
-                                                  y:centerPoint.y,
-                                                  width: lineWidth,
-                                                  height: lineHeight
-            ))
-            
-            let transform = CGAffineTransform(translationX: bounds.width/2 - diameter/2 ,y: bounds.height/2 - diameter/2)
-            
-            guageTickPath.apply(transform)
             
             let percent = CGFloat(CGFloat(index)/CGFloat(numberOfOuterLines))
             let angle = (percent * 2 * pi)
             let colorIndex = index % 12
             let radius = diameter / 2 - margin
+            
             print ("Percentage of index,percent, pi, degree \(index), \(percent), \(angle), \(angle * 57.2958 ), \(colorIndex)")
-            colors[colorIndex].setFill()
-            //            UIColor.red.setFill()
-            guageTickPath.fill()
+            //            colors[colorIndex].setFill()
+            UIColor.black.setFill()
+            let guageTickPath = UIBezierPath(rect: CGRect(x: centerPoint.x,
+                                                          y:centerPoint.y,
+                                                          width: lineWidth,
+                                                          height: lineHeight
+            ))
+            let transform = CGAffineTransform(translationX: bounds.width/2 - diameter/2 ,y: bounds.height/2 - diameter/2)
+            guageTickPath.apply(transform)
             
             context?.translateBy(x: centerPoint.x, y: centerPoint.y)
             context?.rotate(by: angle)
             context?.translateBy(x: -centerPoint.x, y: -centerPoint.y)
+            guageTickPath.fill()
+            
+            context?.restoreGState()
+            context?.saveGState()
         }
+        context?.restoreGState()
     }
     
 }
