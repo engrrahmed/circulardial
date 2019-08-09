ec16098b264d752d8f357c8e162b0375ce31e2bc
Rizwan Ahmed <rizwan.ahmed@tintash.com>
2019-08-09 17:46:46 +0500

    Added code mark + method to draw tick using rect (previous method)
    What: added code mark in the circle view class + added previous method which draw tick using Rect (bezier path)
    Why:  added previous method for comparison +. Added Mark and comments for code readability and notes
    Impact: low, it will not affect other part of the code

--- TemperatureDial/TemperatureDial/CircularView/CircularView.swift
+++ TemperatureDial/TemperatureDial/CircularView/CircularView.swift
@@ -11,6 +11,8 @@ let pi = CGFloat(Double.pi)
 
 @IBDesignable
 class CircularView: UIView {
+    
+    // MARK:- Class variable
     //start angle: where we need to start the reading scale
     //Stop angle: where we need to start the reading scale
     @IBInspectable var startAngle           :CGFloat    = 0.0
@@ -22,13 +24,17 @@ class CircularView: UIView {
     //line width of the tick
     @IBInspectable var markWidth            :CGFloat    = 2.0
     //line height of the tick
-    @IBInspectable var markHeight           :CGFloat    = 20.0
+    @IBInspectable var markLength           :CGFloat    = 20.0
     
     // pointing to current value of the guage
     var currentValueMarkIndex               :Int        = 10 //0
-    var baseValueMarkIndex                  :Int        = 2 //0
+    var baseValueMarkIndex                  :Int        {
+        get {
+            return numberOfOuterLines / 2
+        }
+    }
     //tempdial related variables
-    var markIndexOfLightColor               :Int        = 10
+    var markIndexOfLightColor               :Int        = 0
     var alphaValueForLightMark              :CGFloat    = 0.4
     
     
@@ -36,7 +42,8 @@ class CircularView: UIView {
     //TODO: added for testing purpose only
     let colors:[UIColor] = [.red, .green, .blue, .red, .green, .blue, .red, .green, .blue,  .red, .green, .blue]
     
-    
+
+    // MARK:- UIView draw method
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
@@ -44,16 +51,23 @@ class CircularView: UIView {
         let context = UIGraphicsGetCurrentContext()
         //Added outer circle for testing purpose only
         addOuterCircle(rect: rect)
+        
+        //DialRect: in which dial needs to be drawn and keeping margin from the outer bound of the view.
         let dialRect = rect.insetBy(dx: margin * 1.5, dy: margin * 1.5)
         addOuterCircle(rect: dialRect)
+        //created mark tick from Line using UIBezierPath
         addOuterLines(rect: dialRect, context: context)
+        //created mark tick from rect using UIBezierPath
+        //        addGuageLines(context: context, rect: dialRect)
     }
     
 }
 
 
 
+// MARK:-  Drawing methods
 extension CircularView {
+    // MARK:- Create outer circle of the dial
     func addOuterCircle (rect: CGRect) {
         let diameter = min(rect.width, rect.height)
         let circle = UIBezierPath(ovalIn: CGRect(x: 0 , y: 0,
@@ -69,6 +83,8 @@ extension CircularView {
         UIColor.black.setStroke()
         circle.stroke()
     }
+    
+    // MARK:- Create text on the mark tick
     
     func drawText(name: String, centerPoint:CGPoint, radius:CGFloat, angle:CGFloat) {
         let context = UIGraphicsGetCurrentContext()
@@ -96,6 +112,7 @@ extension CircularView {
         context?.restoreGState()
     }
     
+    // MARK:- Create mark tick using line
     func addOuterLines(rect: CGRect, context : CGContext?) {
         
         let centerPoint = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
@@ -105,14 +122,14 @@ extension CircularView {
         for index in 0..<numberOfOuterLines {
             
             var lineWidth   : CGFloat = markWidth
-            var lineHeight  : CGFloat = markHeight
+            var lineLength  : CGFloat = markLength
             
             if index == baseValueMarkIndex  || index == currentValueMarkIndex {
                 lineWidth = 4.0
             }
             
             if index == currentValueMarkIndex {
-                lineHeight = markHeight * 1.6
+                lineLength = markLength * 1.6
             }
             
             let percent = CGFloat(CGFloat(index )/CGFloat(numberOfOuterLines))
@@ -121,8 +138,8 @@ extension CircularView {
             let path = UIBezierPath()
             path.lineWidth = lineWidth
             
-            let starPoint = CGPoint(x: -lineHeight / 2, y: 0)
-            let endPoint = CGPoint(x: lineHeight / 2 , y: 0)
+            let starPoint = CGPoint(x: -lineLength / 2, y: 0)
+            let endPoint = CGPoint(x: lineLength / 2 , y: 0)
             path.move(to: endPoint)
             path.addLine(to: starPoint)
             
@@ -132,15 +149,60 @@ extension CircularView {
             path.lineCapStyle = .round
             //            UIColor.red.setStroke()
             let colorIndex = index % 12
-            colors[colorIndex].setStroke()
+            UIColor.black.setStroke()
+//            colors[colorIndex].setStroke()
             
             context?.saveGState()
-            //            context?.translateBy(x: centerPoint.x - lineHeight  / 2 * cos(angle) * 0 , y: centerPoint.y - lineHeight / 2 * sin(angle)  * 0)
+            //            context?.translateBy(x: centerPoint.x - lineLength  / 2 * cos(angle) * 0 , y: centerPoint.y - lineLength / 2 * sin(angle)  * 0)
             context?.translateBy(x: centerPoint.x, y: centerPoint.y)
             context?.rotate(by: angle)
             path.stroke()
             context?.restoreGState()
-            drawText(name: "\(index)", centerPoint: centerPoint, radius:radius  + lineHeight / 2 + textMargin, angle:angle)
+            drawText(name: "\(index)", centerPoint: centerPoint, radius:radius  + lineLength / 2 + textMargin, angle:angle)
         }
+    }
+}
+
+// MARK:- Create mark tick using rect
+extension CircularView  {
+    func addGuageLines(context : CGContext?, rect: CGRect) {
+        let diameter = min(rect.width, rect.height)
+        let centerPoint = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+        context?.saveGState()
+        for index in 0..<numberOfOuterLines {
+            
+            let percent = CGFloat(CGFloat(index)/CGFloat(numberOfOuterLines))
+            let angle = (percent * 2 * pi)
+            let colorIndex = index % 12
+            let radius = diameter / 2
+            
+            print ("Percentage of index,percent, pi, degree \(index), \(percent), \(angle), \(angle * 57.2958 ), \(colorIndex)")
+            //            colors[colorIndex].setFill()
+            UIColor.black.setFill()
+            var lineWidth = markWidth
+            if index == baseValueMarkIndex ||  index == currentValueMarkIndex{
+                lineWidth  = markWidth * 2
+            }
+            let lineLength = (index == currentValueMarkIndex) ? markLength * 1.5 : markLength
+            let markYpos = (index == currentValueMarkIndex) ? centerPoint.y -  markLength / 4 : centerPoint.y
+            let guageTickPath = UIBezierPath(rect: CGRect(x:        centerPoint.x,
+                                                          y:        markYpos,
+                                                          width:    lineLength,
+                                                          height:   lineWidth
+            ))
+            let transform = CGAffineTransform(translationX: radius  - lineLength / 2,y: radius * 0 )
+            guageTickPath.apply(transform)
+            context?.saveGState()
+            context?.translateBy(x: centerPoint.x, y: centerPoint.y)
+            context?.rotate(by: angle)
+            context?.translateBy(x: -centerPoint.x, y: -centerPoint.y)
+            let markAlphaValue:CGFloat = (index < markIndexOfLightColor) ?  alphaValueForLightMark : 1.0
+            guageTickPath.fill(with: .xor, alpha: markAlphaValue)
+            
+            context?.restoreGState()
+            
+            drawText(name: "\(index)", centerPoint: centerPoint, radius: radius  + lineLength / 2 + textMargin, angle:angle)
+        }
+        context?.restoreGState()
     }
 }
